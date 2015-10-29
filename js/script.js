23b8c14450c0873c0b3d43c141abba145efb86fb
Hectormm <hectormm@gmail.com>
2015-10-29 16:17:25 +0100

    cambio carpeta

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 (function() {
 	console.log('Recuperando Stop Words');	
-    $.getJSON('util/es.json', function(data) {
+    $.getJSON('utiles/es.json', function(data) {
         var stopWords = data;
         console.log(data);
     });
