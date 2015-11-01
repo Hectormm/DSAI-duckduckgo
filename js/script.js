9839ca4cf941a74a05abbc83d72892c1b531d2b7
Hectormm <hectormm@gmail.com>
2015-11-01 19:09:27 +0100

    comprobacion

--- js/script.js
+++ js/script.js
@@ -86,74 +86,80 @@ function palabrasMasRepetidas() {
 function buscarPalabras() {
     $("#resultado").append("<hr>");
 
-    for (var i = 0; i < 3 && i < frecuentes.length; i++) {
-        $.ajax({
-            url: 'https://api.duckduckgo.com/?q=' + frecuentes[i].id + '&format=json',
-            dataType: 'jsonp',
-            success: function(data) {
-
-                //Solo si encuentra resultados de la palabra
-                if (data.Heading !== '') {
-                    $("#resultado").append('<div class="col-lg-9">' + '<p>' +
-                        '<strong>' + data.Heading + '</strong> - ' +
-                        '<a href=' + data.AbstractURL + '>' + data.AbstractURL + '</a>' + ' <br>' + data.Abstract +
-                        '</p>' +
-                        '</div>');
-                    //Si existe la imagen
-                    if (data.Image !== '') {
-                        $("#resultado").append('<div class="col-lg-3">' +
-                            '<p>' +
-                            '<img src="' + data.Image + '" alt="' + data.Heading + '" height="150" width="150"> ' +
-                            '</p>' +
-                            '</div>');
-                    }
-
-                    //Para cada related topic
-                    _.each(data.RelatedTopics, function(ele, it) {
-                        var contador = it + 1;
-
-                        if (ele.Result && ele.Text) {
-                            $("#resultado").append('<div class="col-lg-offset-1 col-lg-11">' +
-                                '<p>' + '<strong>' + contador + ' </strong>' + ele.Result + ele.Text +
-                                '</p>' +
-                                '</div>');
-                        } else {
-                            $("#resultado").append('<div class="col-lg-offset-1 col-lg-11">' +
-                                '<p>' + '<strong>' + contador + ' </strong>' + ele.Name +
-                                '</p>' +
-                                '</div>');
-
-                            _.each(ele, function(ele2, cont){
-                            	var contador2 = cont + 1;
-		                        $("#resultado").append('<div class="col-lg-offset-2 col-lg-10">' +
-		                            '<p>' + '<strong>' + contador + '.' + contador2 + ' </strong>' + ele2.Result + ele2.Text +
-		                            '</p>' +
-		                            '</div>');
-                            });
-                        }
-
-                    });
-
-                    $("#resultado").append('<div class="col-lg-12"> <hr> </div>');
-                }
-                //Si no ha encontrado resultados
-                var contenido = $("#resultado").html();
-                if (contenido === '<hr>' && (i === 2 || i === frecuentes.length)) {
-                    $("#resultado").append('<div class="col-lg-12" style="text-align:center;"> <h3>No se han encontrado resultados </h3></div>');
-                }
-                console.log(data);
-            },
-            error: function() {
-                console.log("Ha ocurrido un error");
-            }
-        });
-
-
-    };
-
     if ($("#texto").val() === '') {
         $("#resultado").append('<div class="col-lg-12" style="text-align:center;"> <h3>Introduzca un texto </h3></div>');
+    } else {
+
+        for (var i = 0; i < 3 && i < frecuentes.length; i++) {
+            $.ajax({
+                url: 'https://api.duckduckgo.com/?q=' + frecuentes[i].id + '&format=json',
+                dataType: 'jsonp',
+                success: function(data) {
+
+                    //Solo si encuentra resultados de la palabra
+                    if (data.Heading !== '') {
+                        $("#resultado").append('<div class="col-lg-9">' + '<p>' +
+                            '<strong>' + data.Heading + '</strong> - ' +
+                            '<a href=' + data.AbstractURL + '>' + data.AbstractURL + '</a>' + ' <br>' + data.Abstract +
+                            '</p>' +
+                            '</div>');
+                        //Si existe la imagen
+                        if (data.Image !== '') {
+                            $("#resultado").append('<div class="col-lg-3">' +
+                                '<p>' +
+                                '<img src="' + data.Image + '" alt="' + data.Heading + '" height="150" width="150"> ' +
+                                '</p>' +
+                                '</div>');
+                        }
+
+                        //Para cada related topic
+                        _.each(data.RelatedTopics, function(ele, it) {
+                            var contador = it + 1;
+
+                            if (ele.Result && ele.Text) {
+                                $("#resultado").append('<div class="col-lg-offset-1 col-lg-11">' +
+                                    '<p>' + '<strong>' + contador + ' </strong>' + ele.Result + ele.Text +
+                                    '</p>' +
+                                    '</div>');
+                            } else {
+                                $("#resultado").append('<div class="col-lg-offset-1 col-lg-11">' +
+                                    '<p>' + '<strong>' + contador + ' </strong>' + ele.Name +
+                                    '</p>' +
+                                    '</div>');
+
+                                _.each(ele, function(ele2, cont) {
+                                    var contador2 = cont + 1;
+                                    $("#resultado").append('<div class="col-lg-offset-2 col-lg-10">' +
+                                        '<p>' + '<strong>' + contador + '.' + contador2 + ' </strong>' + ele2.Result + ele2.Text +
+                                        '</p>' +
+                                        '</div>');
+                                });
+                            }
+
+                        });
+
+                        $("#resultado").append('<div class="col-lg-12"> <hr> </div>');
+                    }
+                    //Si no ha encontrado resultados
+                    var contenido = $("#resultado").html();
+                    if (contenido === '<hr>' && (i === 2 || i === frecuentes.length)) {
+                        $("#resultado").append('<div class="col-lg-12" style="text-align:center;"> <h3>No se han encontrado resultados </h3></div>');
+                    }
+                    console.log(data);
+                },
+                error: function() {
+                    console.log("Ha ocurrido un error");
+                }
+            });
+
+
+        };
+
+
     }
+
+
+
 
 }
 
